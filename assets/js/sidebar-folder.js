c926e114fac0a29b6ce391cb62a017cfc965d141
JaeSeong Park <a86007681@gmail.com>
2021-08-08 18:48:29 +0900

    [debug 21-08-08]

--- assets/js/sidebar-folder.js
+++ assets/js/sidebar-folder.js
@@ -1,7 +1,7 @@
 function spread(count){
   document.getElementById('folder-checkbox-' + count).checked =
   !document.getElementById('folder-checkbox-' + count).checked
-  document.getElementById('spread-icon-' + count).style.content =
-  document.getElementById('spread-icon-' + count).style.content == '\f077' ?
-  '\f078' : '\f077'
+  document.getElementById('spread-icon-' + count).innerHTML =
+  document.getElementById('spread-icon-' + count).innerHTML == '▼' ?
+  '▲' : '▼'
 }
